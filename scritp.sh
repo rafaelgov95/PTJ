aba892fb19ca531647c39f8240fa8c26d34be441
Rafael Viana <rafael.viana@aluno.ufms.br>
2017-10-30 23:57:22 -0300

    Update scritp.sh

--- scritp.sh
+++ scritp.sh
@@ -4,7 +4,6 @@ java -jar CliPTJ-1.0-SNAPSHOT.jar 12
 java -jar CliPTJ-1.0-SNAPSHOT.jar 12
 java -jar CliPTJ-1.0-SNAPSHOT.jar 12
 java -jar CliPTJ-1.0-SNAPSHOT.jar 12
-java -jar CliPTJ-1.0-SNAPSHOT.jar 12
 java -jar CliPTJ-1.0-SNAPSHOT.jar #P#
 java -jar CliPTJ-1.0-SNAPSHOT.jar 12
 java -jar CliPTJ-1.0-SNAPSHOT.jar 12
